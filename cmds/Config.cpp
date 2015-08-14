1bfeb120d1b5c5a834f13d8c94c851d650d16d5c
xaizek <xaizek@openmailbox.org>
2015-08-14 16:23:35 +0300

    Add "config" command
    Gets/sets configuration.

---
+++ cmds/Config.cpp
@@ -0,0 +1,96 @@
+// Copyright (C) 2015 xaizek <xaizek@openmailbox.org>
+//
+// This file is part of scribe.
+//
+// scribe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// scribe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with scribe.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <cstdlib>
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "Command.hpp"
+#include "Commands.hpp"
+#include "Config.hpp"
+#include "Project.hpp"
+#include "utils.hpp"
+
+namespace {
+
+/**
+ * @brief Implementation of "config" command, which gets/sets configuration.
+ */
+class ConfigCmd : public Command
+{
+public:
+    /**
+     * @brief Constructs the command implementation.
+     */
+    ConfigCmd();
+
+public:
+    /**
+     * @copydoc Command::run()
+     */
+    virtual int run(Project &project,
+                    const std::vector<std::string> &args) override;
+};
+
+REGISTER_COMMAND(ConfigCmd);
+
+}
+
+ConfigCmd::ConfigCmd()
+    : Command("config", "read/update configuration",
+              "Usage: config [key[=val]...]")
+{
+}
+
+int
+ConfigCmd::run(Project &project, const std::vector<std::string> &args)
+{
+    if (args.empty()) {
+        return EXIT_FAILURE;
+    }
+
+    Config &config = project.getConfig();
+
+    for (const std::string &a : args) {
+        bool set;
+        std::string key, value;
+        try {
+            std::tie(key, value) = splitAt(a, '=');
+            set = true;
+        } catch (std::runtime_error &) {
+            set = false;
+            key = a;
+        }
+
+        if (key.empty() || key[0] == '!') {
+            std::cout << "Wrong key name: '" << key << "'\n";
+            continue;
+        }
+
+        if (set) {
+            config.set(key, value);
+        } else {
+            std::cout << key << " = " << config.get(key, "<not set>") << '\n';
+        }
+    }
+    std::cout.flush();
+
+    return EXIT_SUCCESS;
+}
