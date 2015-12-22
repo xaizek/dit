3ac51e69c7d2ba5f91d2c7f6e4daa8c94fc2e08d
xaizek <xaizek@openmailbox.org>
2015-12-22 17:35:21 +0200

    Add "export" command
    Item data exporter, which passes fields to an external script.

---
+++ src/cmds/ExportCmd.cpp
@@ -0,0 +1,192 @@
+// Copyright (C) 2015 xaizek <xaizek@openmailbox.org>
+//
+// This file is part of dit.
+//
+// dit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// dit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with dit.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <unistd.h>
+
+#include <cstdlib>
+
+#include <algorithm>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/find_format.hpp>
+#include <boost/algorithm/string/finder.hpp>
+
+#include "Command.hpp"
+#include "Commands.hpp"
+#include "Item.hpp"
+#include "ItemFilter.hpp"
+#include "Project.hpp"
+#include "completion.hpp"
+
+/**
+ * @brief Usage message for "export" command.
+ */
+const char *const USAGE = R"(Usage: export cmd [expr like for ls...]
+
+The cmd is run once for each item with arguments of the form key=value.)";
+
+namespace {
+
+/**
+ * @brief Implementation of "export" command, which passes item data to command.
+ */
+class ExportCmd : public AutoRegisteredCommand<ExportCmd>
+{
+public:
+    /**
+     * @brief Constructs the command implementation.
+     */
+    ExportCmd();
+
+public:
+    /**
+     * @copydoc Command::run()
+     *
+     * @throws std::runtime_error On external command failure.
+     */
+    virtual boost::optional<int> run(
+        Project &project,
+        const std::vector<std::string> &args) override;
+    /**
+     * @copydoc Command::complete()
+     */
+    virtual boost::optional<int> complete(
+        Project &project,
+        const std::vector<std::string> &args) override;
+
+private:
+    /**
+     * @brief Invokes the command for given item.
+     *
+     * @param cmd Beginning of command line to be constructed.
+     * @param item Source of item data.
+     *
+     * @throws std::runtime_error On external command failure.
+     */
+    void exportItem(const std::string &cmd, Item &item);
+    /**
+     * @brief Escapes special shell characters in a string.
+     *
+     * @param str String to escape.
+     *
+     * @returns Escaped string.
+     */
+    std::string shellEscape(std::string str);
+};
+
+/**
+ * @brief Command-line escaping helper.
+ */
+class ShellEscaper
+{
+public:
+    /**
+     * @brief Escapes range of characters.
+     *
+     * @tparam T Type of range.
+     * @param match Range itself.
+     *
+     * @returns Escaped range.
+     */
+    template <typename T>
+    std::string operator()(const T& match) const
+    {
+        std::ostringstream oss;
+        for (const auto &c : match) {
+            oss << '\\' << c;
+        }
+        return oss.str();
+    }
+};
+
+}
+
+ExportCmd::ExportCmd() : parent("export", "item data exporter", USAGE)
+{
+}
+
+boost::optional<int>
+ExportCmd::run(Project &project, const std::vector<std::string> &args)
+{
+    if (args.empty()) {
+        err() << "Expected at least one argument.\n";
+        return EXIT_FAILURE;
+    }
+
+    const std::string &cmd = args.front();
+    ItemFilter filter({ args.cbegin() + 1, args.cend() });
+
+    for (Item &item : project.getStorage().list()) {
+        if (filter.passes(item)) {
+            exportItem(cmd, item);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+boost::optional<int>
+ExportCmd::complete(Project &project, const std::vector<std::string> &args)
+{
+    if (args.size() <= 1U) {
+        return EXIT_SUCCESS;
+    }
+    return completeKeys(project.getStorage(), out());
+}
+
+void
+ExportCmd::exportItem(const std::string &cmd, Item &item)
+{
+    std::ostringstream cmdLine;
+
+    // Compose command-line.
+    cmdLine << cmd;
+    for (const std::string &key : item.listRecordNames()) {
+        cmdLine << ' ' << key << '=' << shellEscape(item.getValue(key));
+    }
+
+    // Execute the command-line.
+    int status = std::system(cmdLine.str().c_str());
+    bool success = (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS);
+
+    // Throw an exception on failure.
+    if (!success) {
+        throw std::runtime_error("Exporter client returned an error.");
+    }
+}
+
+std::string
+ExportCmd::shellEscape(std::string str)
+{
+    // Turn newlines into spaces and let export client wrap the result if
+    // needed.
+    // XXX: might not be the best solution, but will do for most use cases.
+    std::replace(str.begin(), str.end(), '\n', ' ');
+
+    // Escape characters that have special meaning for shells.
+    boost::find_format_all(
+        str,
+        boost::token_finder(
+            boost::is_any_of("\\\r\n\t'\"; ?|[]{}<>`!$&*()#~")),
+        ShellEscaper());
+
+    return std::move(str);
+}
---
+++ tests/cmds/ExportCmd.cpp
@@ -0,0 +1,129 @@
+// Copyright (C) 2015 xaizek <xaizek@openmailbox.org>
+//
+// This file is part of dit.
+//
+// dit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// dit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with dit.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "Catch/catch.hpp"
+
+#include <cstdlib>
+
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+
+#include "Change.hpp"
+#include "Command.hpp"
+#include "Commands.hpp"
+#include "Item.hpp"
+#include "Project.hpp"
+
+#include "Tests.hpp"
+
+TEST_CASE("Export fails on wrong invocation", "[cmds][export][invocation]")
+{
+    std::unique_ptr<Project> prj = Tests::makeProject();
+    Command *const cmd = Commands::get("export");
+
+    std::ostringstream out;
+    std::ostringstream err;
+    Tests::setStreams(out, err);
+
+    boost::optional<int> exitCode;
+
+    SECTION("No arguments")
+    {
+        exitCode = cmd->run(*prj, {});
+    }
+
+    REQUIRE(exitCode);
+    REQUIRE(*exitCode == EXIT_FAILURE);
+
+    REQUIRE(out.str() == std::string());
+    REQUIRE(err.str() != std::string());
+}
+
+TEST_CASE("Completion of export", "[cmds][export][completion]")
+{
+    std::unique_ptr<Project> prj = Tests::makeProject();
+    Storage &storage = prj->getStorage();
+
+    Item item = Tests::makeItem("id");
+    item.setValue("title", "title");
+    item.setValue("bug_number", "22");
+
+    Tests::storeItem(storage, std::move(item));
+
+    Command *const cmd = Commands::get("export");
+
+    std::ostringstream out;
+    std::ostringstream err;
+    Tests::setStreams(out, err);
+
+    boost::optional<int> exitCode;
+    std::string expectedOut;
+
+    SECTION("First argument is not completed")
+    {
+        exitCode = cmd->complete(*prj, { "ti" });
+    }
+
+    SECTION("Second argument is completed")
+    {
+        exitCode = cmd->complete(*prj, { "cmd", "ti" });
+        expectedOut =
+            "title\n"
+            "bug_number\n";
+    }
+
+    REQUIRE(exitCode);
+    REQUIRE(*exitCode == EXIT_SUCCESS);
+
+    REQUIRE(out.str() == expectedOut);
+    REQUIRE(err.str() == std::string());
+}
+
+TEST_CASE("Item exporting", "[cmds][export]")
+{
+    std::unique_ptr<Project> prj = Tests::makeProject();
+    Storage &storage = prj->getStorage();
+
+    Item item = Tests::makeItem("id");
+    item.setValue("title", "This's a title");
+    item.setValue("bug_number", "22");
+
+    Tests::storeItem(storage, std::move(item));
+
+    Command *const cmd = Commands::get("export");
+
+    std::ostringstream out;
+    std::ostringstream err;
+    Tests::setStreams(out, err);
+
+    SECTION("No operation run")
+    {
+        boost::optional<int> exitCode = cmd->run(*prj, { ":" });
+        REQUIRE(exitCode);
+        REQUIRE(*exitCode == EXIT_SUCCESS);
+    }
+
+    SECTION("Failed run")
+    {
+        REQUIRE_THROWS_AS(cmd->run(*prj, { "no-such-cmd" }),
+                          std::runtime_error);
+    }
+
+    REQUIRE(out.str() == std::string());
+    REQUIRE(err.str() == std::string());
+}
