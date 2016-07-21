b5570f46fdcb9d2ebb65a1ddd24c031f06222d33
xaizek <xaizek@openmailbox.org>
2016-07-21 12:53:57 +0300

    Make "show" accept list of fields to display

--- src/cmds/ShowCmd.cpp
+++ src/cmds/ShowCmd.cpp
@@ -20,6 +20,7 @@
 #include <algorithm>
 #include <ostream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 #include "utils/strings.hpp"
@@ -73,15 +74,16 @@ private:
 
 }
 
-ShowCmd::ShowCmd() : parent("show", "display item entries", "Usage: show id")
+ShowCmd::ShowCmd() : parent("show", "display item entries",
+                            "Usage: show id [key...]")
 {
 }
 
 boost::optional<int>
 ShowCmd::run(Project &project, const std::vector<std::string> &args)
 {
-    if (args.size() != 1) {
-        err() << "Expected single argument (id).\n";
+    if (args.size() < 1U) {
+        err() << "Expected at least one argument (id).\n";
         return EXIT_FAILURE;
     }
 
@@ -91,10 +93,15 @@ ShowCmd::run(Project &project, const std::vector<std::string> &args)
     const std::vector<std::string> ordering = split(order, ',');
 
     const std::string &id = args[0];
+    std::unordered_set<std::string> filter(++args.cbegin(), args.cend());
     Item &item = project.getStorage().get(id);
 
     // Print known fields in specified order first.
     for (const std::string &field : ordering) {
+        if (!filter.empty() && !contains(filter, field)) {
+            continue;
+        }
+
         const std::string &v = item.getValue(field);
         if (!v.empty()) {
             printRecord(field, v);
@@ -103,6 +110,10 @@ ShowCmd::run(Project &project, const std::vector<std::string> &args)
 
     // Print out the rest of the fields.
     for (const std::string &c : item.listRecordNames()) {
+        if (!filter.empty() && !contains(filter, c)) {
+            continue;
+        }
+
         if (!contains(ordering, c)) {
             printRecord(c, item.getValue(c));;
         }
@@ -136,10 +147,15 @@ ShowCmd::printRecord(const std::string &name, const std::string &val)
 boost::optional<int>
 ShowCmd::complete(Project &project, const std::vector<std::string> &args)
 {
-    if (args.size() > 1) {
-        return EXIT_FAILURE;
+    if (args.size() <= 1U) {
+        return completeIds(project.getStorage(), out());
     }
 
-    completeIds(project.getStorage(), out());
-    return EXIT_SUCCESS;
+    try {
+        const std::string &id = args[0];
+        Item &item = project.getStorage().get(id);
+        return completeKeys(item, out(), { ++args.cbegin(), args.cend() });
+    } catch (std::runtime_error &) {
+        return EXIT_FAILURE;
+    }
 }
