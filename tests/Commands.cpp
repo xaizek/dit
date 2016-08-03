0b7307e019c7636b718b467385e60a1781c201eb
xaizek <xaizek@openmailbox.org>
2016-08-03 21:45:02 +0300

    Get rid of side effects in Commands::get()

---
+++ tests/Commands.cpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2016 xaizek <xaizek@openmailbox.org>
+//
+// This file is part of dit.
+//
+// dit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// dit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with dit.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "Catch/catch.hpp"
+
+#include <cstddef>
+
+#include "Commands.hpp"
+
+TEST_CASE("Get doesn't have side effects", "[commands]")
+{
+    const std::size_t commandCount = Commands::list().size();
+
+    REQUIRE(Commands::get("doesn't exist") == nullptr);
+    REQUIRE(Commands::list().size() == commandCount);
+}
